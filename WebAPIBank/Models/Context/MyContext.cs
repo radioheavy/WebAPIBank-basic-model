b9116a6ea06df0cae3803484ddddd179108eb219
İsmail Oktay Dak <dakoktaytgb@gmail.com>
2022-12-05 17:40:56 +0300

    Add project files.

---
+++ WebAPIBank/Models/Context/MyContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WebAPIBank.Models.Entites;
+using WebAPIBank.Models.Init;
+
+namespace WebAPIBank.Models.Context
+{
+    public class MyContext : DbContext
+    {
+        public MyContext() : base("MyContext")
+        {
+            Database.SetInitializer(new MyInit());
+        }
+        public DbSet<CardInfo> Cards { get; set; }
+    }
+}
---
+++ WebAPIBank/Models/Init/MyInit.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Dynamic;
+using System.Linq;
+using System.Web;
+using WebAPIBank.Models.Context;
+using WebAPIBank.Models.Entites;
+
+namespace WebAPIBank.Models.Init
+{
+    public class MyInit : CreateDatabaseIfNotExists<MyContext>
+    {
+        protected override void Seed(MyContext context)
+        {
+            CardInfo cardInfo = new CardInfo();
+            cardInfo.CardUserName = "Oktay Dak";
+            cardInfo.CardNumber = "1111 1111 1111 1111";
+            cardInfo.CardExpiryYear = 2028;
+            cardInfo.CardExpiryMonth = 10;
+            cardInfo.SecurityNumber = "111";
+            cardInfo.Limit = 500000;
+            cardInfo.Balance = 5000000;
+            context.Cards.Add(cardInfo);
+            context.SaveChanges();
+
+        }
+    }
+}
